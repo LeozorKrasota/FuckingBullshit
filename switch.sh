61b0c4668788b4d86e00c738d425699e26a7c135
LeozorKrasota <118885914+LeozorKrasota@users.noreply.github.com>
2025-08-15 12:45:55 +0300

    Create switch.sh

---
+++ switch.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+#move to script directory so all relative paths work
+cd "$(dirname "$0")"
+
+#includes
+. ./config.sh
+. ./environment.sh
+
+if [ .$switch_source = .true ]; then
+	if [ ."$switch_branch" = "master" ]; then
+		verbose "Start script switch/source-master.sh"
+		switch/source-master.sh
+	else
+		verbose "Start script switch/source-release.sh"
+		switch/source-release.sh
+	fi
+
+	#add sounds and music files
+	switch/source-sounds.sh
+
+	#copy the switch conf files to /etc/freeswitch
+	switch/conf-copy.sh
+
+	#set the file permissions
+	#switch/source-permissions.sh
+	switch/package-permissions.sh
+
+	#systemd service
+	#switch/source-systemd.sh
+	switch/package-systemd.sh
+fi
+
+if [ .$switch_package = .true ]; then
+	if [ ."$switch_branch" = "master" ]; then
+		if [ .$switch_package_all = .true ]; then
+			verbose "Start script switch/package-master-all.sh"
+			switch/package-master-all.sh
+		else
+			verbose "Start script switch/package-master.sh"
+			switch/package-master.sh
+		fi
+	else
+		if [ .$switch_package_all = .true ]; then
+			verbose "Start script switch/package-all.sh"
+			switch/package-all.sh
+		else
+			verbose "Start script switch/package-release.sh"
+			switch/package-release.sh
+		fi
+	fi
+
+	#copy the switch conf files to /etc/freeswitch
+	switch/conf-copy.sh
+
+	#set the file permissions
+	switch/package-permissions.sh
+
+	#systemd service
+	switch/package-systemd.sh
+fi
